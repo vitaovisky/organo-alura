2f401d2016ef4db0f255fee92b2719d27c490998
vitaovisky <vitao5013@gmail.com>
2024-10-03 09:52:17 -0300

    Adicionando pós debug

--- src/App.js
+++ src/App.js
@@ -47,7 +47,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) =>{
-    debugger
     setColaboradores([...colaboradores, colaborador])
   }
 
