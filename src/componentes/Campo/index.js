99917c853a4cf22bbc4480ee1b6658acdb711cb1
vitaovisky <vitao5013@gmail.com>
2024-10-07 23:40:59 -0300

    Estilizando componente de campo

---
+++ src/componentes/Campo/index.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './Campo.css'
+
+const Campo = ({type = 'texto', label, placeholder, valor, aoAlterado, obrigatorio = false }) => {
+
+    const aoDigitado = (evento) =>{
+    aoAlterado(evento.target.value)
+}
+    return(
+        <div className={`campo campo-${type}`}>
+            <label>{label}</label>
+            <input 
+                type={type} 
+                value={valor} 
+                onChange={aoDigitado} 
+                required={obrigatorio} 
+                placeholder={placeholder}
+            />
+        </div>
+    )
+}
+
+export default Campo
